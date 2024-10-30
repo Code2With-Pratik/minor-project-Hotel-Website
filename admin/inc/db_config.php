d52ae93f6be2224bf620f6fbb88238cd449eb517
Code2With-Pratik <work.pratik1204@gmail.com>
2024-10-30 19:29:29 +0530

    db_config.php file created and the login form connected with the data base with no error

---
+++ admin/inc/db_config.php
@@ -0,0 +1,16 @@
+<?php
+
+ $hname = 'localhost';
+ $uname = 'root';
+ $pass = '';
+ $db = 'hotelwebsite';
+
+ $con = mysqli_connect($hname,$uname,$pass,$db);
+
+ if(!$con){
+    die("Cannot Connect to Database" .mysqli_connect_errno());
+ }
+
+
+
+?>
