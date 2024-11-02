3639be6944d4f3d06efa70d04ea26b96b7772a0f
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-02 12:21:33 +0530

    general setting model updated and data can be fetch from data base

--- admin/settings.php
+++ admin/settings.php
@@ -16,9 +16,9 @@
 <?php require('inc/header.php'); ?>
 
 
-  <div class="container-fluid" id="main-content" >
-     <div class="row">
-        <div class="col-lg-10 ms-auto p-4 over-hidden">
+ <div class="container-fluid" id="main-content" >
+   <div class="row">
+      <div class="col-lg-10 ms-auto p-4 over-hidden">
             <h3 class="mb-4 " >SETTINGS</h3>
 
                 <!-- General settings section -->
@@ -143,7 +143,6 @@
                     </div>
                   </div>
                 </div>
-            
               </div>
             </div>
 
