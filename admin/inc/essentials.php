f533fb5d3e3eb34ca25d200f47e04df003fcfae8
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-03 18:42:13 +0530

    function declare as uploadImage

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -33,5 +33,29 @@ function alert($type,$msg){
      alert;
 }
 
+function uploadImage($image,$folder)
+{
+  $valid_mime = ['image/jpeg','image/png','image/webp'];
+  $img_mime = $image['type'];
+
+  if(in_array($img_mime,$valid_mime)){
+    return 'inv_img';  // invalid image mime or format
+  }
+    else if(($image['size']/(1024*1024))>2){
+      return 'inv_size'; // invalid size greater than 2mb
+    }
+    else{
+        $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+        $rname = 'IMG_'.random_int(11111,99999).".$ext";
+        
+        $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+        if(move_uploaded_file($image['tmp_name'],$img_path)){
+            return $rname;
+        }
+        else{
+            return 'upd_failed';
+        }
+    }
+}
 
 ?>
