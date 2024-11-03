d466cd9ddcbd15172a0b30213127f330a423f4fd
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-03 18:44:57 +0530

    insert function declare to fetch data dynamically from DataBase

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -41,6 +41,7 @@
         die("Query cannot be prepared - Select");
     }
  }
+
  function update($sql,$values,$datatypes)
  {
     $con = $GLOBALS['con'];
@@ -59,6 +60,27 @@
     }
     else{
         die("Query cannot be prepared - Update");
+    }
+ }
+
+ function insert($sql,$values,$datatypes)
+ {
+    $con = $GLOBALS['con'];
+    if($stmt = mysqli_prepare($con,$sql))
+    {
+       mysqli_stmt_bind_param($stmt,$datatypes,...$values); 
+       if(mysqli_stmt_execute($stmt)){
+         $res = mysqli_stmt_affected_rows($stmt);
+         mysqli_stmt_close($stmt);
+         return $res;
+       }
+       else{
+        mysqli_stmt_close($stmt);
+        die("Query cannot be executed - Insert");
+       }
+    }
+    else{
+        die("Query cannot be prepared - Insert");
     }
  }
 
