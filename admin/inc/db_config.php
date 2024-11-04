bc282f0a6d54539c024dafa3c4171a40c5b56797
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-04 22:41:02 +0530

    get_member function regenerated

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -1,96 +1,104 @@
 <?php
-
- $hname = 'localhost';
- $uname = 'root';
- $pass = '';
- $db = 'hotelwebsite';
-
- $con = mysqli_connect($hname,$uname,$pass,$db);
-
- if(!$con){
-    die("Cannot Connect to Database" .mysqli_connect_errno());
- }
-
- function filteration($data){
-    foreach($data as $key => $value){
-       $data[$key] = trim($value);
-       $data[$key] = stripcslashes($value);
-       $data[$key] = htmlspecialchars($value);
-       $data[$key] = strip_tags($value);
-    }
-       return $data;
- }
-
- function selectAll($table)
- {
-   $con = $GLOBALS('con');
-   $res = mysqli_query($con,"SELECT * FROM $table");
-   return $res;
- }
-
- function select($sql,$values,$datatypes)
- {
-    $con = $GLOBALS['con'];
-    if($stmt = mysqli_prepare($con,$sql))
-    {
-       mysqli_stmt_bind_param($stmt,$datatypes,...$values); 
-       if(mysqli_stmt_execute($stmt)){
-         $res = mysqli_stmt_get_result($stmt);
-         mysqli_stmt_close($stmt);
-         return $res;
-       }
-       else{
-        mysqli_stmt_close($stmt);
-        die("Query cannot be executed - Select");
-       }
-    }
-    else{
-        die("Query cannot be prepared - Select");
-    }
- }
-
- function update($sql,$values,$datatypes)
- {
-    $con = $GLOBALS['con'];
-    if($stmt = mysqli_prepare($con,$sql))
-    {
-       mysqli_stmt_bind_param($stmt,$datatypes,...$values); 
-       if(mysqli_stmt_execute($stmt)){
-         $res = mysqli_stmt_affected_rows($stmt);
-         mysqli_stmt_close($stmt);
-         return $res;
-       }
-       else{
-        mysqli_stmt_close($stmt);
-        die("Query cannot be executed - Update");
-       }
-    }
-    else{
-        die("Query cannot be prepared - Update");
-    }
- }
  
- function insert($sql,$values,$datatypes)
- {
-    $con = $GLOBALS['con'];
-    if($stmt = mysqli_prepare($con,$sql))
-    {
-       mysqli_stmt_bind_param($stmt,$datatypes,...$values); 
-       if(mysqli_stmt_execute($stmt)){
-         $res = mysqli_stmt_affected_rows($stmt);
-         mysqli_stmt_close($stmt);
-         return $res;
-       }
-       else{  
-        mysqli_stmt_close($stmt);
-        die("Query cannot be executed - Insert");
-       }
-    }
-    else{
-        die("Query cannot be prepared - Insert");
-    }
- }
- 
+    require('../inc/db_config.php');
+    require('../inc/essentials.php');
+    adminLogin();
 
+    if(isset($_POST['get_general']))
+    {
+        $q = "SELECT * FROM `settings` WHERE `sr_no`=?";
+        $values = [1];
+        $res = select($q,$values,"i");
+        $data = mysqli_fetch_assoc($res);
+        $json_data = json_encode($data);
+        echo $json_data;
+    }
+
+    if(isset($_POST['upd_general']))
+    {
+        $frm_data = filteration($_POST);
+
+        $q = "UPDATE `settings` SET `site_title`=?,`site_about`=? WHERE `sr_no`=?";
+        $values = [$frm_data['site_title'],$frm_data['site_about'],1];
+        $res = update($q,$values,'ssi');
+        echo $res;
+    }
+
+
+    if(isset($_POST['upd_shutdown']))
+    {
+        $frm_data = ($_POST['upd_shutdown']==0) ? 1 : 0;
+
+        $q = "UPDATE `settings` SET `shutdown`=? WHERE `sr_no`=?";
+        $values = [$frm_data,1];
+        $res = update($q,$values,'ii');
+        echo $res;
+    }
+
+    if(isset($_POST['get_contacts']))
+    {
+        $q = "SELECT * FROM `contact_details` WHERE `sr_no`=?";
+        $values = [1];
+        $res = select($q,$values,"i");
+        $data = mysqli_fetch_assoc($res);
+        $json_data = json_encode($data);
+        echo $json_data;
+    }
+
+    if(isset($_POST['upd_contacts']))
+    {
+        $frm_data = filteration($_POST);
+
+        $q = "UPDATE `contact_details` SET `address`=?,`gmap`=?,`pn1`=?,`pn2`=?,`email`=?,`fb`=?,`insta`=?,`tw`=?,`iframe`=? WHERE `sr_no`=?";
+        $values = [$frm_data['address'],$frm_data['gmap'],$frm_data['pn1'],$frm_data['pn2'],$frm_data['email'],$frm_data['fb'],$frm_data['insta'],$frm_data['tw'],$frm_data['iframe'],1];
+        $res = update($q,$values,'sssssssssi');
+        echo $res;
+    }
+
+    if(isset($_POST['add_member']))
+    {
+        $frm_data = filteration($_POST);
+
+        $img_r = uploadImage($_FILES['picture'],ABOUT_FOLDER);
+
+        if($img_r == 'inv_img'){
+            echo $img_r;
+        }
+        else if($img_r == 'inv_size'){
+            echo $img_r;
+        }
+        else if($img_r == 'upd_failed'){
+            echo $img_r;
+        }
+        else{
+            $q = "INSERT INTO `team_details`(`name`, `picture`) VALUES (?,?)";
+            $values = [$frm_data['name'],$img_r];
+            $res = insert($q,$values,'ss');
+            echo $res;
+        }
+    }
+ 
+    if(isset($_POST['get_members']))
+    {
+        $res = selectAll('team_detais');
+        
+        while($row = mysqli_fetch_assoc($res))
+        {
+            $path = ABOUT_IMG_PATH;
+            echo <<<data
+                <div class="col-md-2 mb-3">
+                    <div class="card bg-dark text-white">
+                        <img src="$path$row[picture]" class="card-img">
+                        <div class="card-img-overlay text-end">
+                            <button type="button" class="btn btn-danger btn-sm shadow-none">
+                                <i class="bi bi-trash"></i> Delete
+                            </button>
+                        </div>
+                        <p class="card-text text-center px-3 py-2">$row[name]</p>
+                    </div>
+                </div>   
+            data;
+        }
+    }
 
 ?>
