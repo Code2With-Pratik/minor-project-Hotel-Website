b4e26093348f3a743c70ff57e58c912c999bb8dd
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-05 17:39:37 +0530

    db_config.php code debuged

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -13,13 +13,12 @@
 
  function filteration($data){
     foreach($data as $key => $value){
-      $value = trim($value);
-      $value = stripslashes($value);
-      $value = strip_tags($value);
-      $value = htmlspecialchars($value);
-      $data[$key] = $value;
+       $value = trim($value);
+       $value = stripslashes($value);
+       $value = strip_tags($value);
+       $value = htmlspecialchars($value);
+       $data[$key] = $value;
     }
-    
        return $data;
  }
 
