fd5833848594d4d393ac729387675e94c7715ffa
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-08 21:48:25 +0530

    empty spaces removed

--- admin/features_facilities.php
+++ admin/features_facilities.php
@@ -71,7 +71,7 @@
 
                <!-- features  section -->
 
- <div class="container-fluid" id="main-content" >
+<div class="container-fluid" id="main-content" >
    <div class="row">
       <div class="col-lg-10 ms-auto p-4 over-hidden">
             <h3 class="mb-4 " >FEATURES & FACILITIES</h3>
