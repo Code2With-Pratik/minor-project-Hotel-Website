b65842d3020a4852df6759b9ed9dd8a2216ec948
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-09 23:46:24 +0530

    del and seen function remove

--- admin/features_facilities.php
+++ admin/features_facilities.php
@@ -2,59 +2,6 @@
   require('inc/essentials.php');
   require('inc/db_config.php');
   adminLogin();
-
-        // for marked as read button 
-  if(isset($_GET['seen']))
-  {
-    $frm_data = filteration($_GET);
-
-    if($frm_data['seen']=='all'){
-      $q = "UPDATE `user_queries` SET `seen`=?";
-      $values = [1];
-      if(update($q,$values,'i')){
-        alert('success','Marked all as read!');
-      }
-      else{
-        alert('error','Operation Failed!');
-      }
-    }
-    else{
-      $q = "UPDATE `user_queries` SET `seen`=? WHERE `sr_no`=?";
-      $values = [1,$frm_data['seen']];
-      if(update($q,$values,'ii')){
-        alert('success','Marked as read!');
-      }
-      else{
-        alert('error','Operation Failed!');
-      }
-    }
-  }
-
-        // for delete button
-  if(isset($_GET['del']))
-  {
-    $frm_data = filteration($_GET);
-
-    if($frm_data['del']=='all'){
-      $q = "DELETE FROM `user_queries`";
-      if(mysqli_query($con,$q)){
-        alert('success','All data deleted!');
-      }
-      else{
-        alert('error','Operation Failed!');
-      }
-    }
-    else{
-      $q = "DELETE FROM `user_queries` WHERE `sr_no`=?";
-      $values = [$frm_data['del']];
-      if(delete($q,$values,'i')){
-        alert('success','Data deleted!');
-      }
-      else{
-        alert('error','Operation Failed!');
-      }
-    }
-  }
 ?>
 
 <!DOCTYPE html>
