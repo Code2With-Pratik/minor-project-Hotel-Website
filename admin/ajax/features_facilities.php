3b4e301a7a9ab8f740b819c22a6ae4d8373f89ac
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-09 23:12:33 +0530

    get_facility and rem_facility function created

--- admin/ajax/features_facilities.php
+++ admin/ajax/features_facilities.php
@@ -46,28 +46,47 @@
         $res = delete($q,$values,'i'); 
         echo $res;
     }
-
-    if(isset($_POST['add_facility']))
-    {
+    
+    if (isset($_POST['add_facility'])) {
         $frm_data = filteration($_POST);
-
-        $img_r = uploadSVGImage($_FILES['icon'],FEATURES_FOLDER);
-
-        if($img_r == 'inv_img'){
-            echo $img_r;
-        }
-        else if($img_r == 'inv_size'){
-            echo $img_r;
-        }
-        else if($img_r == 'upd_failed'){
-            echo $img_r;
-        }
-        else{
-            $q = "INSERT INTO `facilities`(`icon`, `name`, `description`) VALUES (?,?,?)";
-            $values = [$img_r,$frm_data['name'],$frm_data['desc']];
-            $res = insert($q,$values,'sss');
-            echo $res;
+    
+        $q = "INSERT INTO `facilities`(`name`, `description`) VALUES (?, ?)";
+        $values = [$frm_data['name'], $frm_data['desc']]; // Use 'desc' to match JavaScript
+        $res = insert($q, $values, 'ss');
+        echo $res;
+    }
+    
+    if (isset($_POST['get_facility'])) {
+        $res = selectAll('facilities'); 
+        $i = 1;
+    
+        // Output each facility in a table row
+        while ($row = mysqli_fetch_assoc($res)) {
+            echo <<<data
+                <tr>
+                    <td>{$i}</td>
+                    <td>{$row['name']}</td>
+                    <td>{$row['description']}</td>
+                    <td>
+                        <button type="button" onclick="rem_facility({$row['id']})" class="btn btn-danger btn-sm shadow-none">
+                            <i class="bi bi-trash"></i> Delete
+                        </button>
+                    </td>
+                </tr>
+            data;
+            $i++;
         }
     }
+
+    if(isset($_POST['rem_facility']))
+    {
+        $frm_data = filteration($_POST);
+        $values = [$frm_data['rem_facility']];
+        
+        $q = "DELETE FROM `facilities` WHERE `id`=?";
+        $res = delete($q,$values,'i'); 
+        echo $res;
+    }
+    
 
 ?>
