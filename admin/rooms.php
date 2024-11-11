f2dde8bdcd495f143f6356be0d8f274394c380ed
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-11 22:15:02 +0530

    To get the data from the database of rooms created. the get function.

--- admin/rooms.php
+++ admin/rooms.php
@@ -207,19 +207,7 @@
     xhr.send(data);
   }
 
-  function get_all_rooms()
-  {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST","ajax/rooms.php",true);
-
-    xhr.onload = function(){
-     document.getElementById('room-data').innerHTML = this.responseText;
-     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    };
-
-    xhr.send('get_all_rooms');
-  }
-
+ 
   window.onload = function(){
     get_all_rooms();
   }
