65ee8f56a13f36bdc106b3b81f6d20eb3599f08d
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-11 21:58:34 +0530

    User queries.php page added to header

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -25,6 +25,9 @@
               <li class="nav-item">
                 <a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="user_queries.php">User Queries</a>
+              </li>
             </ul>
           </div>
         </div>
