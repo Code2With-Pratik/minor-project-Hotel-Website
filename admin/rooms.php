a48151aa8ffc3b7c986ecba25c6ba74581f441a5
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-11 22:11:50 +0530

    For rooms page, the database is created.

--- admin/rooms.php
+++ admin/rooms.php
@@ -156,56 +156,7 @@
     add_room();
   });
 
-  function add_room()
-  {
-    let data = new FormData();
-    data.append('add_room','');
-    data.append('name',add_room_form.elements['name'].value);
-    data.append('area',add_room_form.elements['area'].value);
-    data.append('price',add_room_form.elements['price'].value);
-    data.append('quantity',add_room_form.elements['quantity'].value);
-    data.append('adult',add_room_form.elements['adult'].value);
-    data.append('children',add_room_form.elements['children'].value);
-    data.append('desc',add_room_form.elements['desc'].value);
-
-
-    let features = [];
-    add_room_form.elements['features'].forEach(el => {
-      if(el.checked){
-        features.push(el.value);
-      }
-    });
-    
-    let facilities = [];
-    add_room_form.elements['facilities'].forEach(el => {
-      if(el.checked){
-        facilities.push(el.value);
-      }
-    });
-
-    data.append('features',JSON.stringify(features));
-    data.append('facilities',JSON.stringify(facilities));
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST","ajax/rooms.php",true);
-
-    xhr.onload = function(){
-      var myModal = document.getElementById('add-room');
-      var modal = bootstrap.Modal.getInstance(myModal);
-      modal.hide();
-
-      if(this.responseText == 1){
-      alert('success','New room added!');
-      add_room_form.reset();
-
-      }
-      else{
-      alert('error','Server Down!');
-      }
-    }
-
-    xhr.send(data);
-  }
+  
 
   function get_all_rooms()
   {
