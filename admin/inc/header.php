5a6899aa50450db360650ed993b22e3d5aa51472
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-11 21:57:25 +0530

    features and facilities.php page added to header

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -22,6 +22,9 @@
               <li class="nav-item">
                 <a class="nav-link text-white" href="rooms.php">Rooms</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a>
+              </li>
             </ul>
           </div>
         </div>
