b2ca2e90d00588a502861ec709a35ae502e6c0a0
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-12 19:21:52 +0530

    toggle_status function created

--- admin/rooms.php
+++ admin/rooms.php
@@ -224,8 +224,26 @@
     xhr.send('get_all_rooms');
   }
 
+  function toggle_status(id,val)
+  {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST","ajax/rooms.php",true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onload = function(){
+      if(this.responseText==1){
+        alert('success', 'Status toggled!');
+      }
+      else{
+        alert('error', 'Server Down!');
+      }
+    }
+
+    xhr.send('toggle_status='+id+'$value='+val);
+  }
+
   window.onload = function(){
-    get_all_rooms();
+    toggle_status();
   }
 
 
