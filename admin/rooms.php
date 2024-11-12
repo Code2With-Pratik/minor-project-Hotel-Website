2a670e5a5984eeb9889e0d83d3720d87f96808b3
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-12 20:11:52 +0530

    edit model created

--- admin/rooms.php
+++ admin/rooms.php
@@ -150,6 +150,95 @@
   </div>
 </div>
 
+                 <!-- Edit-rooms-Modal -->
+<div class="modal fade" id="edit-room" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <form id="add_room_form" autocomplete="off" >
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Add Rooms</h5>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-md-6 mb-3 ">
+              <label class="form-label fw-bold">Name</label>
+              <input type="text" name="name" class="form-control shadow-none" required >
+            </div>                
+            <div class="col-md-6 mb-3 ">
+              <label class="form-label fw-bold">Area</label>
+              <input type="number" min="1" name="area" class="form-control shadow-none" required >
+            </div>                
+            <div class="col-md-6 mb-3 ">
+              <label class="form-label fw-bold">Guests</label>
+              <input type="number" min="1" name="guests" class="form-control shadow-none" required >
+            </div>                
+            <div class="col-md-6 mb-3 ">
+              <label class="form-label fw-bold">Price</label>
+              <input type="number" min="1" name="price" class="form-control shadow-none" required >
+            </div>                
+            <div class="col-md-6 mb-3 ">
+              <label class="form-label fw-bold">Quantity</label>
+              <input type="number" min="1" name="quantity" class="form-control shadow-none" required >
+            </div>                
+            <div class="col-md-6 mb-3 ">
+              <label class="form-label fw-bold">Adults (Max.)</label>
+              <input type="number" min="1" name="adult" class="form-control shadow-none" required >
+            </div>                
+            <div class="col-md-6 mb-3 ">
+              <label class="form-label fw-bold">Children (Max.)</label>
+              <input type="number" min="1" name="children" class="form-control shadow-none" required >
+            </div> 
+            <div class=" col-12 mb-3">
+            <label class="form-label fw-bold">Features</label>
+            <div class="row">
+              <?php
+                $res = selectAll('features');
+                while($opt = mysqli_fetch_assoc($res)){
+                  echo"
+                    <div class='col-md-3 mb-1'>
+                       <label>
+                          <input type='checkbox' name='features' value='$opt[id]' class='form-check-input shadow-none'>
+                          $opt[name]
+                       </label>
+                    </div>
+                  ";
+                }
+              ?>
+            </div>
+            </div>               
+            <div class=" col-12 mb-3">
+            <label class="form-label fw-bold">Facilities</label>
+            <div class="row">
+              <?php
+                $res = selectAll('facilities');
+                while($opt = mysqli_fetch_assoc($res)){
+                  echo"
+                    <div class='col-md-3 mb-1'>
+                       <label>
+                          <input type='checkbox' name='facilities' value='$opt[id]' class='form-check-input shadow-none'>
+                          $opt[name]
+                       </label>
+                    </div>
+                  ";
+                }
+              ?>
+            </div>
+            </div>
+            <div class="col-12 mb-3">
+              <label class="form-label fw-bold">Description</label>
+              <textarea name="desc" rows="4" class="form-control shadow-none" required></textarea>
+            </div>               
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="reset" class="btn text-secondary shadow-none" data-bs-dismiss="modal" >CANCEL</button>
+          <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
 
 <?php require('inc/scripts.php'); ?> 
 <script>
