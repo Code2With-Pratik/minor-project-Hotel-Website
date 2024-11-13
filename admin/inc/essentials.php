4f0fd9c4e5fafcece4d942025e886f0f9697c0f6
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-13 20:29:39 +0530

    rooms images path constant declared

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -5,13 +5,16 @@
     define('SITE_URL','http://127.0.0.1/hotelwebsite/');
     define('ABOUT_IMG_PATH',SITE_URL.'images/about/');
     define('CAROUSEL_IMG_PATH',SITE_URL.'images/carousel/');
-    define('FEATURES_IMG_PATH',SITE_URL.'images/features/');
+    define('FACILITIES_IMG_PATH',SITE_URL.'images/facilities/');
+    define('ROOMS_IMG_PATH',SITE_URL.'images/rooms/');
 
     // Backend upload process needs this data 
 
 define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/minor project/images/');
 define('ABOUT_FOLDER','about/');
-define('FEATURES_FOLDER','features/');
+define('CAROUSEL_FOLDER','carousel/');
+define('FACILITIES_FOLDER','facilities/');
+define('ROOMS_FOLDER','rooms/');
 
 function adminLogin()
 {
