fcb188c0dcbfcf154d83dadf05614b25b4b9e8c2
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-16 21:24:36 +0530

    line 277 removed

--- rooms.php
+++ rooms.php
@@ -90,7 +90,7 @@
           }
 
           // get facilities of rooms
-          
+
           $fac_q = mysqli_query($con,"SELECT f.name FROM `facilities` f 
           INNER JOIN `room_facilities` rfac ON f.id = rfac.facilities_id 
           WHERE rfac.room_id = '$room_data[id]'");
@@ -273,7 +273,6 @@
     
   </div>
 </div>
-
 
 
  <?php require('inc/footer.php');?>
