83535303444ed0811eab6994fd06e78b19bdf722
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-17 12:56:49 +0530

    id changed too roomCarousel

--- room_details.php
+++ room_details.php
@@ -40,8 +40,8 @@
               </div>
             </div>
 
-            <div class="col-lg-7 col-md-12 px-4 bg-dark">
-              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="col-lg-7 col-md-12 px-4">
+              <div id="roomCarousel" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
                     <img src="..." class="d-block w-100" alt="...">
@@ -53,11 +53,11 @@
                     <img src="..." class="d-block w-100" alt="...">
                   </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <button class="carousel-control-prev" type="button" data-bs-target="#roomCarousel" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <button class="carousel-control-next" type="button" data-bs-target="#roomCarousel" data-bs-slide="next">
                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Next</span>
                 </button>
