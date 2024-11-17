0208db26c8750d7eb71438f3d459ec146a5fd64d
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-17 16:29:13 +0530

    facilities fetchh dynamically

--- index.php
+++ index.php
@@ -319,26 +319,19 @@
 
 <div class="container">
   <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
-    <div class="col-lg-2 col-md-2; text-center bg-white rounded shadow py-4 my-3">
-      <img src="images/features/wifi.svg" width="80px" alt="">
-      <h5 class="mt-3">WiFi</h5>
-    </div>
-    <div class="col-lg-2 col-md-2; text-center bg-white rounded shadow py-4 my-3">
-      <img src="images/features/wifi.svg" width="80px" alt="">
-      <h5 class="mt-3">WiFi</h5>
-    </div>
-    <div class="col-lg-2 col-md-2; text-center bg-white rounded shadow py-4 my-3">
-      <img src="images/features/wifi.svg" width="80px" alt="">
-      <h5 class="mt-3">WiFi</h5>
-    </div>
-    <div class="col-lg-2 col-md-2; text-center bg-white rounded shadow py-4 my-3">
-      <img src="images/features/wifi.svg" width="80px" alt="">
-      <h5 class="mt-3">WiFi</h5>
-    </div>
-    <div class="col-lg-2 col-md-2; text-center bg-white rounded shadow py-4 my-3">
-      <img src="images/features/wifi.svg" width="80px" alt="">
-      <h5 class="mt-3">WiFi</h5>
-    </div> 
+  <?php 
+        $res = selectAll('facilities');
+
+        while($row = mysqli_fetch_assoc($res)){
+          echo<<<data
+              <div class="col-lg-2 col-md-2; text-center bg-white rounded shadow py-4 my-3">
+                <img src="images/features/wifi.svg" width="80px" alt="">
+                <h5 class="mt-3">$row[name]</h5>
+              </div>
+          data;
+        }
+      ?>
+
     <div class="col-lg-12 text-center mt-5">
        <a href="facilities.php" class="btn btn-sm btn-outline-dark rounded-1 fw-bold shadow-none">More Facilities >>></a>
     </div>
