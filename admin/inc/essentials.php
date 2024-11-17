e4708ec1c3ccfa7b84b50a2c4ce6a025003bd92b
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-17 21:14:53 +0530

    new function created as uploaduserimage

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -10,11 +10,12 @@
 
     // Backend upload process needs this data 
 
-define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/minor project/images/');
+define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/minor project/images/');
 define('ABOUT_FOLDER','about/');
 define('CAROUSEL_FOLDER','carousel/');
 define('FACILITIES_FOLDER','facilities/');
 define('ROOMS_FOLDER','rooms/');
+define('USERS_FOLDER','users/');
 
 function adminLogin()
 {
@@ -49,7 +50,7 @@ function alert($type,$msg){
 function uploadImage($image,$folder)
 {
   $valid_mime = ['image/jpeg','image/png','image/webp'];
-  $img_mime = $image('type');
+  $img_mime = $image['type'];
 
   if(!in_array($img_mime,$valid_mime)){
     return 'inv_img';  // invalid image or format
@@ -98,6 +99,39 @@ function uploadSVGImage($image,$folder)
 
     $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
     if(move_uploaded_file($image['tmp_name'],$img_path)){
+        return $rname;
+    }
+    else{
+        return 'upd_failed';
+    }
+  }
+}
+
+function uploadUserImage($image)
+{
+  $valid_mime = ['image/jpeg','image/png','image/webp'];
+  $img_mime = $image['type'];
+
+  if(!in_array($img_mime,$valid_mime)){
+    return 'inv_img';  // invalid image or format
+  }
+  else{
+    $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+    $rname = 'IMG_'.random_int(11111,99999).".jpeg";
+
+    $img_path = UPLOAD_IMAGE_PATH.USERS_FOLDER.$rname;
+
+    if($ext == 'png' || $ext == 'PNG'){
+      $img = imagecreatefrompng($image['tmp_name']);
+    }
+    else if($ext == 'webp' || $ext == 'WEBP') {
+      $img = imagecreatefromwebp($image['tmp_name']);
+    }
+    else{
+      $img = imagecreatefromjpeg($image['tmp_name']);
+    }
+    
+    if(imagejpeg($img,$img_path,75)){
         return $rname;
     }
     else{
