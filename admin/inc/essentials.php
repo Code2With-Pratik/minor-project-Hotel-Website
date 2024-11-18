a654d648af9c85f723d3512f0ca6031425093ec0
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-18 18:37:57 +0530

    corrected indent

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -10,12 +10,12 @@
 
     // Backend upload process needs this data 
 
-define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/minor project/images/');
-define('ABOUT_FOLDER','about/');
-define('CAROUSEL_FOLDER','carousel/');
-define('FACILITIES_FOLDER','facilities/');
-define('ROOMS_FOLDER','rooms/');
-define('USERS_FOLDER','users/');
+    define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/minor project/images/');
+    define('ABOUT_FOLDER','about/');
+    define('CAROUSEL_FOLDER','carousel/');
+    define('FACILITIES_FOLDER','facilities/');
+    define('ROOMS_FOLDER','rooms/');
+    define('USERS_FOLDER','users/');
 
 function adminLogin()
 {
