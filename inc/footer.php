5c1632f4b461ef0a95ac7afee5cda489edc5aae4
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-18 18:44:58 +0530

    .

--- inc/footer.php
+++ inc/footer.php
@@ -41,6 +41,7 @@
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <script>
+      
       function setActive()
       {
         let navbar = document.getElementById('nav-bar');
@@ -93,5 +94,5 @@
       });
 
       setActive();
-      
+
     </script>
