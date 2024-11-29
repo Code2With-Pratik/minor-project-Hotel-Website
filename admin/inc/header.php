5e5dd9b58e5c7e111ca19f1143331c0fad666425
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-29 15:18:52 +0530

    Update header.php Name changed and about updated

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,8 +1,8 @@
         <!-- admin pannel logout button -->
 
 <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
-  <h3 class="mb-0 h-font">Hotel Website</h3>
-  <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+    <h3 class="mb-0 h-font">GrandStay Website</h3>
+    <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
 </div>
 
         <!-- admin pannel sidebar -->
@@ -20,13 +20,29 @@
                 <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
               </li>
               <li class="nav-item">
+                <button class="btn text-white px-3 w-100 shadow-none text-start d-flex align-items-center justify-content-between" type="button" data-bs-toggle="collapse" data-bs-target="#bookingLinks"x>
+                  <span>Bookings</span>
+                  <span><i class="bi bi-caret-down-fill"></i></span>
+                </button>
+                <div class="collapse show px-3 small mb-1" id="bookingLinks">
+                  <ul class="nav nav-pills flex-column rounded border border-secondary">
+                    <li class="nav-item">
+                      <a class="nav-link text-white" href="new_bookings.php">New Bookings</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="users.php">Users</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="user_queries.php">User Queries</a>
+              </li>
+              <li class="nav-item">
                 <a class="nav-link text-white" href="rooms.php">Rooms</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="user_queries.php">User Queries</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link text-white" href="carousel.php">Carousel</a>
