679a78241952813fad883dc991fe857098f71658
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-29 15:37:02 +0530

    Update user_queries.php

--- admin/user_queries.php
+++ admin/user_queries.php
@@ -63,6 +63,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - User Queries</title>
+    <link rel="icon" href="../images/logo/hotel-logo.avif">
     <?php require('inc/links.php'); ?>
 </head>
 <body class="bg-light">
