14906ee332879b7a416019d1d650da28c83e19de
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-29 14:58:22 +0530

    script added into the footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,11 +1,10 @@
 <div class="container-fluid bg-white mt-5">
    <div class="row">
     <div class="col-lg-4 p-4">
-        <h3 class="h-font fw-bold fs-3 mb-2">Hotel</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Iste quas optio voluptas.</p>
-           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Placeat, cumque.
+        <h3 class="h-font fw-bold fs-3 mb-2">GrandStay</h3>
+        <p>Experience Grandeur, Every Day Where Luxury Meets Comfort</br>
+        Experience Grandeur, Every Day .Affordable Comfort, Anytime and Smart Stays for Savvy Travelers
+       . Simplify Your Stay </p>
     </div>
     <div class="col-lg-4 p-4">
           <h5 mb-3>link</h5>
@@ -36,65 +35,115 @@
    </div>
 </div>
 
-<h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by @code_2_with_pratik</h6>
+   <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by @code_2_with_pratik</h6>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
-crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-<script>
+    <script>
 
-  function setActive()
-  {
-    let navbar = document.getElementById('nav-bar');
-    let a_tags = navbar.getElementsByTagName('a');
 
-    for(i=0; i<a_tags.length; i++){
-      let file = a_tags[i].href.split('/').pop();
-      let file_name = file.split('.')[0];
+      function alert(type,msg){
+        let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
+        let element = document.createElement('div');
+        element.innerHTML = `
+            <div class="alert ${bs_class} alert-dismissible fade show custom-alert" role="alert">
+                <strong class="me-3" >${msg}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `;
+        document.body.append(element);
+        setTimeout(remAlert, 2000);
+     }
 
-      if(document.location.href.indexOf(file_name) >= 0){
-        a_tags[i].classList.add('active');
+      function remAlert(){
+          document.getElementsByClassName('alert')[0].remove();
       }
 
 
-    }
-  }
+      function setActive()
+      {
+        let navbar = document.getElementById('nav-bar');
+        let a_tags = navbar.getElementsByTagName('a');
 
-  
-  let register_form =document.getElementById('register-form');
+        for(i=0; i<a_tags.length; i++){
+          let file = a_tags[i].href.split('/').pop();
+          let file_name = file.split('.')[0];
 
-  register_form.addEventListener('submit', (e)=>{
+          if(document.location.href.indexOf(file_name) >= 0){
+            a_tags[i].classList.add('active');
+          }
+
+
+        }
+      }
+
+      
+     // Register form submit event (AJAX)
+  let register_form = document.getElementById('register-form');
+  register_form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    let data = new FormData();
+    let data = new FormData(register_form);
+    data.append('register', '');
 
-    data.append('name',register_form.elements['name'].value);
-    data.append('email',register_form.elements['email'].value);
-    data.append('phonenum',register_form.elements['phonenum'].value);
-    data.append('address',register_form.elements['address'].value);
-    data.append('pincode',register_form.elements['pincode'].value);
-    data.append('dob',register_form.elements['dob'].value);
-    data.append('pass',register_form.elements['pass'].value);
-    data.append('cpass',register_form.elements['cpass'].value);
-    data.append('profile',register_form.elements['profile'].files[0]);
-    data.append('register','');
-
-    var myModal = document.getElementById('registerModal');
-    var modal = bootstrap.Modal.getInstance(myModal);
+    let myModal = document.getElementById('registerModal');
+    let modal = bootstrap.Modal.getInstance(myModal);
     modal.hide();
 
     let xhr = new XMLHttpRequest();
-    xhr.open("POST","ajax/login_register.php",true);
+    xhr.open("POST", "login_register.php", true);
 
-    xhr.onload = function(){
+    xhr.onload = function () {
+      if (this.responseText === 'pass_mismatch') {
+        alert('error', 'Password Mismatch!');
+      } else if (this.responseText === 'email_already') {
+        alert('error', 'Email is already registered!');
+      } else if (this.responseText === 'phone_already') {
+        alert('error', 'Phone number is already registered!');
+      } else if (this.responseText === 'ins_failed') {
+        alert('error', 'Registration failed! Server down!');
+      } else if (this.responseText === '1') {
+        alert('success', 'Registration successful!');
+        register_form.reset();
+      }
+    };
+    xhr.send(data);
+  });
 
+  // Login form submit event (AJAX)
+  let login_form = document.getElementById('login-form');
+  login_form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let data = new FormData(login_form);
+    data.append('login', '');
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "login_register.php", true);
+
+    xhr.onload = function () {
+      if (this.responseText === 'no_user') {
+        alert('error', 'No user found with this email!');
+      } else if (this.responseText === 'invalid_pass') {
+        alert('error', 'Invalid password!');
+      } else if (this.responseText === '1') {
+        alert('success', 'Login successful!');
+        window.location.reload();
+      } 
     };
 
     xhr.send(data);
-    
   });
 
-  setActive();
 
+  function checkLoginToBook(status,room_id){
+    if(status){
+      window.location.href='confirm_booing.php?id='+room_id;
+    }
+    else{
+      alert('error','Please login to Book Room!');
+    }
+  }
+
+  setActive();
 </script>
